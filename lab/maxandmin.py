97aa5b0fd30eec34bdaa4f340dd577bd183e3779
yashwanthpv2 <yashwanthpvbca24@rvu.edu.in>
2025-01-29 12:00:05 +0530

    Update maxandmin.py

--- lab/maxandmin.py
+++ lab/maxandmin.py
@@ -1,18 +1,10 @@
-def find_largst_and_smallest(arr):
-    if not arr:
-     return none, none #return none if the array is empty
-    largest =arr[0]
-    smallest =arr[0]
-    for num in arr:
-        if num>largest:
-            largest=num 
-        if num<smallest:
-            smallest=num
-                
-    return largest,smallest
-               # example usage:
-array =[10,89,9,56,4,80,8]
-largest,smallest=find_largst_and_smallest(array)
-print("largest element:",largest)
-print("smallest element:",smallest)
- 
+a = [11,77,88,100,87,99]
+min = a[0]
+max = a[0]
+for i in range(0, len(a)):
+    if(min < a[i]):
+        min = a[i]
+    elif(max > a[i]):
+        max = a[i]
+print("Smallest number= ",min)
+print("Largest number= ",max)
